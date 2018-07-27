9c8297738b238d8d4e1b208934cfcd1c3ee14768
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-27 14:55:07 -0600

    switch command implemented to simplify code.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -163,3 +163,20 @@ if(moonPhase==='full' || isFoggyNight ===true){
 } else if (moonPhase === 'mostly new') {
   console.log('Back on two feet.')
 } else {console.log('Invalid moon phase.')}
+
+let moonPhase = 'full';
+
+switch (moonPhase) {
+  case 'full':
+    console.log('Howl!');
+    break;
+  case 'mostly full':
+    console.log('Arms and legs are getting hairier.');
+    break;
+  case 'mostly new':
+    console.log('Back on two feet.');
+    break;
+  default: 
+    console.log('Invalid moon phase.');
+    break;
+}
