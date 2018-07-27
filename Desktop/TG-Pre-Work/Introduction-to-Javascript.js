bb9154a88f35f9b295d024087c130c3dcb306d22
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-26 19:53:30 -0600

    Introduction to using variables.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -46,3 +46,8 @@ let inNorthernHemisphere = true;
 console.log(location);
 console.log(latitude);
 console.log(inNorthernHemisphere);
+const entree = 'Enchiladas';
+const price = 12;
+console.log(entree);
+console.log(price);
+entree = 'Tacos';
