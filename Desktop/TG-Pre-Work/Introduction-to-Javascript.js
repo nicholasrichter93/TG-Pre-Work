c0a0d20245622bd4d75b195eecd46fa50b17f6df
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-26 18:41:39 -0600

    Uses console function to log 4 different types of primitive data.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -1,2 +1,8 @@
 console.log("Banana Peppers!");
 console.log("Hitchhikers Guide to the Universe");
+console.log("Banana Peppers!");
+console.log("Hitchhikers Guide to the Universe");
+console.log('JavaScript');
+console.log(33.7);
+console.log(true);
+console.log(null);
