0eb3d944dd9d3c037e6636bf8dfcfbdee99d2ea8
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-27 16:41:46 -0600

    Beginning of Functions lesson.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -193,3 +193,5 @@ isCorrect ? console.log('Correct!') : console.log('Incorrect!');
 let favoritePhrase = 'Love That!';
 
 favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+
+//Beginning of Functions lesson. 
