3d9449ebca47a15543ba16e463cfcf456f86bcf3
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-27 17:20:09 -0600

    Return function added to declare price of orders.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -226,3 +226,18 @@ const takeOrder = () => {
   };
   
   takeOrder('mushrooms');
+
+  let orderCount = 0;
+const takeOrder = (topping, crustType) => {
+  orderCount++;
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+};
+
+const getSubTotal = (itemCount) => {
+  return itemCount * 7.5;
+}
+
+takeOrder('mushroom', 'thin crust');
+takeOrder('spinach', 'whole wheat');
+takeOrder('pepperoni', 'brooklyn style');
+console.log(getSubTotal(orderCount));
