b5530eefb9780073cadc334c7d9ba9ee3f593448
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-27 15:01:22 -0600

    Changes if/else statements to use ternary operator.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -180,3 +180,16 @@ switch (moonPhase) {
     console.log('Invalid moon phase.');
     break;
 }
+
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
