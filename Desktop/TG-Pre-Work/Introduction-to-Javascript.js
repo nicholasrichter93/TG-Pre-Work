e8ff362677ab638d3f023ac066b43decf1ed25ff
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-27 14:46:40 -0600

    Changes previous operators to logical operators to display output.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -153,3 +153,13 @@ if(moonPhase==='full'){
 } else if (moonPhase === 'mostly new') {
   console.log('Back on two feet.')
 } else {console.log('Invalid moon phase.')}
+
+let moonPhase = 'full';
+let isFoggyNight = false;
+if(moonPhase==='full' || isFoggyNight ===true){
+  console.log('Howl!')
+} else if (moonPhase === 'mostly full'){
+          console.log('Arms and legs are getting hairier.')
+} else if (moonPhase === 'mostly new') {
+  console.log('Back on two feet.')
+} else {console.log('Invalid moon phase.')}
