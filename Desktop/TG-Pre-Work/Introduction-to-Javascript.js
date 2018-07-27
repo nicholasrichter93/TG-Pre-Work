1d01bf6a1e36d3c5366b28a8f67ad21b24b0320f
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-27 14:31:42 -0600

    Uses === operator in an if/else statement to determine output.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -140,3 +140,7 @@ let hungerLevel = 10;
 
 if(hungerLevel > 7){console.log('Time to eat!')} else {console.log('We can eat later!')}
 
+let moonPhase = 'full';
+if(moonPhase==='full'){
+  console.log('Howl!')
+}else {console.log('I swear I am not a werewolf.')}
