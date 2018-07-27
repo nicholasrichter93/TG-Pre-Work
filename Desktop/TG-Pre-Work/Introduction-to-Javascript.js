8bebb74ea55599288b30595592755e7ec3c14c4a
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-27 14:22:53 -0600

    Uses ! to change truthiness of variable.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -119,3 +119,20 @@ if (favoritePhrase) {
   console.log('This string is definitely empty.');
 }
 
+let wordCount = 1;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = 'A';
+
+if (!favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
