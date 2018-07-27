0d80b06fc271f1d66f3661ea536aa894048d5dea
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-27 14:37:52 -0600

    Uses if/else and else/if statements to change console output.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -144,3 +144,12 @@ let moonPhase = 'full';
 if(moonPhase==='full'){
   console.log('Howl!')
 }else {console.log('I swear I am not a werewolf.')}
+
+let moonPhase = 'solar eclipse';
+if(moonPhase==='full'){
+  console.log('Howl!')
+} else if (moonPhase === 'mostly full'){
+          console.log('Arms and legs are getting hairier.')
+} else if (moonPhase === 'mostly new') {
+  console.log('Back on two feet.')
+} else {console.log('Invalid moon phase.')}
