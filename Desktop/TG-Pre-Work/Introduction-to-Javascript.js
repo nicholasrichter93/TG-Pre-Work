cbae8155e772e0adaf686009f9fa04c217c608a0
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-27 14:20:14 -0600

    Changes value of variables to truthy so that message is displayed.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -102,3 +102,20 @@ else{
   console.log('No goal!')
 }
 
+let wordCount = 1;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = 'A';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
