ab679b2d19891226df6a3458503d078ec2ce2860
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-27 20:21:09 -0600

    Creates a variable set to an array.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -350,3 +350,5 @@ const visibleLightWaves = () => {
   let bucketList = ['Rappel into a cave', 'Take a falconry class', 'Learn to juggle'];
 
 console.log(bucketList);
+
+let newYearsResolutions = ['Call my mom more.', 'Eat well.','Make new frieds.'];
