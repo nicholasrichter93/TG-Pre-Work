ae86861095648fdf9bd288475e59353dbcbf868a
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-28 15:45:08 -0600

    Illustrates an example of an infinite loop.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -472,3 +472,13 @@ while (currentCard !== 'Spade') {
 }
 
 console.log('You found a spade!');
+
+let flag = true;
+let counter = 0;
+while(flag === true){
+  console.log(counter);
+  counter+=1;
+  if (counter === 37){
+    break;
+  }
+}
