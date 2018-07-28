2facfe1cdb29afb3c1153b97e7ab2c85f9aa6aa0
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-27 18:59:31 -0600

    Introduction to the for loop.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -335,4 +335,14 @@ const visibleLightWaves = () => {
   };
   
   visibleLightWaves();
+
+  const starCount = () => {
+    let i = 5;
+    console.log(i);
+    for (let i = 0; i < 12; i++){
+      console.log(i);
+    }
+  };
   
+  starCount();
+  console.log(i);
