2edfe6cb9c39a72c0ad4d9cd49bb4f8f8875c233
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-28 15:06:03 -0600

    Explores different methods to edit arrays.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -401,3 +401,13 @@ newYearsResolutions.push('Travel!', 'Floss!');
 console.log(newYearsResolutions);
 
 newYearsResolutions.pop();
+
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+
+groceryList.unshift('popcorn');
+console.log(groceryList);
+
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);
