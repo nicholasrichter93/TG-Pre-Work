16442c076dea5200721adc2c53dea9fdcfb27100
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-28 15:24:19 -0600

    Uses a for loop to log 3 items to the console. Cool!

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -437,3 +437,9 @@ let vacationSpots = ['Thailand', 'Australia', 'Dubai' ]
 console.log(vacationSpots[0]);
 console.log(vacationSpots[1]);
 console.log(vacationSpots[2]);
+
+let vacationSpots = ['Thailand', 'Australia', 'Dubai' ]
+
+for (let vacationSpotIndex = 0; vacationSpotIndex < vacationSpots.length; vacationSpotIndex++) {
+  console.log('I would love to visit ' + vacationSpots[vacationSpotIndex])
+};
