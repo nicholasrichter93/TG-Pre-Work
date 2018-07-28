6c8c72620bb842fed871b8c92c9096b8d719dc8a
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-27 18:54:44 -0600

    Uses block scope in an if function to display block scope properties.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -323,3 +323,16 @@ const visibleLightWaves = () => {
   
   visibleLightWaves();
   console.log(lightWaves);
+
+  const visibleLightWaves = () => {
+    let lightWaves = 'Moonlight';
+    let region = 'The Arctic';
+    if (region === 'The Arctic') {
+      let lightWaves = 'Northern Lights';
+      console.log(lightWaves);
+    }
+    console.log(lightWaves);
+  };
+  
+  visibleLightWaves();
+  
