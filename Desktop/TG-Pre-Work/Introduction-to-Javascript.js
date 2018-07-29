60fc4077f3681a3c6400c26003e690f84d125515
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 13:19:49 -0600

    Creates a getter for age of person object and logs it to the console.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -829,3 +829,24 @@ let person = {
   };
   
   person.age = 39;
+
+  let person = {
+    _name: 'Lu Xun',
+    _age: 137,
+    
+    set age(newAge) {
+      if(typeof newAge === 'number') {
+        this._age = newAge;
+      } else {
+        return 'Invalid input';
+      }
+    },
+    get age() {
+      console.log(`${this._name} is ${this._age} years old.`) 
+      
+    }
+  };
+  
+  person.age = 39;
+  
+  console.log(person.age);
