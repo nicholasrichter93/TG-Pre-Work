633cc7bbd685c0ccb446431dd575a9dba1418ab9
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 14:11:26 -0600

    Calls the .takeVacationDays() method with an input of 5 on new nurse instance and logs remaining vacation days to the console.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -997,4 +997,35 @@ let person = {
   };
   
   const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
+
   
+  class HospitalEmployee {
+    constructor(name) {
+      this._name = name;
+      this._remainingVacationDays = 20;
+    }
+    
+    get name() {
+      return this._name;
+    }
+    
+    get remainingVacationDays() {
+      return this._remainingVacationDays;
+    }
+    
+    takeVacationDays(daysOff) {
+      this._remainingVacationDays -= daysOff;
+    }
+  }
+  
+  class Nurse extends HospitalEmployee {
+   constructor(name, certifications) {
+     super(name);
+     this._certifications = certifications;
+   } 
+  }
+  
+  const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+  
+  nurseOlynyk.takeVacationDays(5);
+  console.log(nurseOlynyk.remainingVacationDays);
