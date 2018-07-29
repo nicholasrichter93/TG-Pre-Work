54c7ec1a35bd669db513aa0a620d61df55ebd241
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 11:18:27 -0600

    Writes a .forEach() iterator for fruits array.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -504,3 +504,11 @@ let onlyNumbers = things.filter(function(thing) {
 });
 
 console.log(onlyNumbers);
+
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+
+fruits.forEach(fruitItem => {
+  console.log('I want to eat a ' + fruitItem);
+});
