e750e745c61848cdace36d3d93dc3edb81650542
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 13:56:09 -0600

    New parent class: HospitalEmployees. Several properties as well as one method added.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -953,3 +953,19 @@ let person = {
   console.log(surgeonCurry.name);
   surgeonCurry.takeVacationDays(3);
   console.log(surgeonCurry.remainingVacationDays);
+
+  class HospitalEmployee {
+    constructor(name){
+      this._name = name;
+      this._remainingVacationDays = 20;
+    }
+    get name() {
+      return this._name;
+    }
+    get remainingVacationDays() {
+      return this._remainingVacationDays;
+    }
+    takeVacationDays(daysOff) {
+      this._remainingVacationDays -= daysOff;
+    }
+  };
