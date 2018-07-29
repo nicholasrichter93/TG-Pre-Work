5593cd714a7d6ae218a0124651559f50a6cc3831
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 11:46:01 -0600

    Calls the .filter function to create a new array based on truthiness of values in the array.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -526,3 +526,14 @@ let bigNumbers = [100, 200, 300, 400, 500];
 // Create the smallNumbers array below
 
 let smallNumbers = bigNumbers.map(num => num/100);
+
+let randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+let smallNumbers = randomNumbers.filter(number => 
+   number < 250);
+
+let favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+let longFavoriteWords = favoriteWords.filter(word => word.length > 7); 
+  
