70d33ca3f3ca4ae43055be0d851e0c52efd19600
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 14:28:44 -0600

    Beginning of Browser Compatibility lesson.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -1164,3 +1164,14 @@ let person = {
       this._insurance = insurance;
     }
   }
+
+  //Beginning of Browser Compatibility lesson. 
+
+  var pasta = "Spaghetti"; // ES5 syntax
+
+const meat = "Pancetta"; // ES6 syntax
+
+let sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
