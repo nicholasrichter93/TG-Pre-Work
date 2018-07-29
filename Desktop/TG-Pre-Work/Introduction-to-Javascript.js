acde22567dbdb34b73f59ada98c394899eb4385f
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 13:23:47 -0600

    Brief introduction to classes.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -852,3 +852,28 @@ let person = {
   console.log(person.age);
 
   //Beginning of Classes lesson. 
+
+  class Dog {
+    constructor(name) {
+      this._name = name;
+      this._behavior = 0;
+    }
+  
+    get name() {
+      return this._name;
+    }
+    get behavior() {
+      return this._behavior;
+    }   
+  
+    incrementBehavior() {
+      this._behavior ++;
+    }
+  }
+  
+  const halley = new Dog('Halley');
+  console.log(halley.name); // Print name value to console
+  console.log(halley.behavior); // Print behavior value to console
+  halley.incrementBehavior(); // Add one to behavior
+  console.log(halley.name); // Print name value to console
+  console.log(halley.behavior); // Print behavior value to console
