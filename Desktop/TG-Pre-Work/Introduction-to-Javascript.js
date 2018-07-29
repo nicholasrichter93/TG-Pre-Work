cb60e9f42a7bcb78c5b4bb0c1a07928efa8978bf
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 12:19:06 -0600

    Adds an if/else statement to determine if an alarm is needed based on the day of the week value.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -591,3 +591,24 @@ let person = {
   
   console.log(person['name']);
   console.log(person['age']);
+
+  let person = {
+    name: 'Nick',
+    age: 24,
+    weekendAlarm: 'No alarms needed',
+    weekAlarm: 'Alarm set to 7AM'
+  };
+  
+  console.log(person['name']);
+  console.log(person['age']);
+  
+  let day = 'Sunday';
+  let alarm;
+  
+  if (day === 'Saturday' || day === 'Sunday' ) {
+    alarm = 'weekendAlarm';
+  } else {
+    alarm = 'weekAlarm';
+  }
+  
+  console.log(person[alarm]);
