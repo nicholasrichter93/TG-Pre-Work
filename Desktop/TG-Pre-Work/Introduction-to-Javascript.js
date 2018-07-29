21ec428923b0bdedae6a2f5e5696ff4f1ca2079c
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 15:25:11 -0600

    Imports airplane.js and creates a .forEach() method for every element.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -1230,3 +1230,13 @@ Airplane.availableAirplanes = [
 ];
 
 export default Airplane;
+
+import Airplane from './airplane';
+
+function displayFuelCapacity() {
+  Airplane.availableAirplanes.forEach(function(element){
+  console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+displayFuelCapacity();
