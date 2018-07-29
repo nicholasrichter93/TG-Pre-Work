8b730453a5b0a20ae608108f4f2cdafed5c9d77a
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 11:52:24 -0600

    Uses .some and .every methods to return new arrays. Fixes bugs in code to return true boolean.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -537,3 +537,18 @@ let favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
 
 let longFavoriteWords = favoriteWords.filter(word => word.length > 7); 
   
+let words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// Something is missing in the method call below
+
+console.log(words.some(function(word) {
+  return word.length < 6;
+}));
+
+// Use filter to create a new array
+
+let interestingWords = words.filter(word => word.length > 5);
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+console.log(interestingWords.every(word =>  word.length > 5      ));
