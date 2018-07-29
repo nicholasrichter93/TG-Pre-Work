a77f92a7ab52b7f4c8245ace8368817dd5997f29
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 15:44:42 -0600

    Removes ties to Airplane module. Uses named imports to import certain variables. Defines a new function that displays staff status.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -1264,3 +1264,21 @@ let availableAirplanes = [{
    };
    
    export { availableAirplanes, flightRequirements, meetsStaffRequirements};
+
+   import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+displayFuelCapacity();
+
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+}
+
+displayStaffStatus();
