6d98f5ea39ff64a2f9650b49300c4b75a0356a25
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 12:30:32 -0600

    Adds a function to the person object to return 'Hello, there!'

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -661,3 +661,32 @@ let person = {
   }
   
   console.log(person[alarm]);
+
+  let person = {
+    name: 'Nick',
+    age: 24,
+    weekendAlarm: 'No alarms needed',
+    weekAlarm: 'Alarm set to 7AM',
+    sayHello: () => {
+      return 'Hello, there!'
+    }
+  };
+  
+  person.hobbies = ['Rock Climbing','Cooking'];
+  person.hobbies = ['Rock Climbing'];
+  
+  console.log(person['name']);
+  console.log(person['age']);
+  console.log(person.hobbies);
+  console.log(person.sayHello());
+  
+  let day = 'Sunday';
+  let alarm;
+  
+  if (day === 'Saturday' || day === 'Sunday' ) {
+    alarm = 'weekendAlarm';
+  } else {
+    alarm = 'weekAlarm';
+  }
+  
+  console.log(person[alarm]);
