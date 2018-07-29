1e531142cd4391b625f90475cc7158c2e3ee54b1
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 13:40:01 -0600

    Creates getters for remaining vacation days and subtracts them from days off.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -895,3 +895,30 @@ let person = {
   const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
   
   const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+  class Surgeon {
+    constructor(name, department) {
+      this._name = name;
+      this._department = department;
+      this._remainingVacationDays = 20;
+    }
+    
+    get name() {
+      return this._name;
+    }
+    
+    get department() {
+      return this._department;
+    }
+    
+    get remainingVacationDays() {
+      return this._remainingVacationDays;
+    }
+    
+    takeVacationDays(daysOff) {
+      this._remainingVacationDays - daysOff;
+    }
+  }
+  
+  const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+  const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
