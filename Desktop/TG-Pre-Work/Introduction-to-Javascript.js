3c590ca8f03fa98a0c011e29e2bd5bbda04158e7
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 15:34:29 -0600

    Adds data to availableAirplanes array and writes a function to determine if there is enough available staff. Exports availableAirplanes, flightRequirements and meetsStaffRequirements.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -1240,3 +1240,27 @@ function displayFuelCapacity() {
 }
 
 displayFuelCapacity();
+
+let availableAirplanes = [{
+    name: 'AeroJet',
+    fuelCapacity: 800,
+    availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+   }, 
+   {name: 'SkyJet',
+    fuelCapacity: 500,
+    availableStaff: ['pilots', 'flightAttendants']
+   }];
+   
+   let flightRequirements = {
+     requiredStaff: 4,
+   };
+   
+   function meetsStaffRequirements(availableStaff, requiredStaff) {
+     if (availableStaff.length >= requiredStaff) {
+       return true;
+     } else {
+       return false;
+     }
+   };
+   
+   export { availableAirplanes, flightRequirements, meetsStaffRequirements};
