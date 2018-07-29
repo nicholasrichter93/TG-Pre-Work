3fb618b7ad4fe656a2f5bf75662f072cb55b700d
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 15:05:11 -0600

    Imports Airplane module via require method and logs myAirplane to the console.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -1197,3 +1197,11 @@ let Airplane = {};
 Airplane.myAirplane = "StarJet";
 
 module.exports = Airplane;
+
+const Airplane = require('./1-airplane.js');
+
+function displayAirplane() {
+  console.log(Airplane.myAirplane);
+};
+
+console.log(displayAirplane());
