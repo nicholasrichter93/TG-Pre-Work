3f05cb0924dfef3d582c220ea3c64a3f9a200a68
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 14:39:57 -0600

    Changes ES6 syntax to ES5.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -1181,3 +1181,12 @@ let esFivePercentageSupport = 96.85;
 
 // Set the variable below to a number
 let esSixTemplateLiterals = 88.72;
+
+var pasta = "Spaghetti"; // ES5 syntax
+
+var meat = "Pancetta"; // ES6 syntax
+
+var sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+var carbonara = 'You can make carbonara with ' + pasta + ',' + meat+ ', and a sauce made with ' + sauce + '.';
