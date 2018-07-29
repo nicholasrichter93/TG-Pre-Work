19b30dd693783c380f79e144915d5bb92ce21ef1
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 12:59:30 -0600

    Creates new object named friend and interpolates function from person object into friend object using 'this'.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -759,3 +759,44 @@ let person = {
   }
   
   console.log(person[alarm]);
+
+  let person = {
+    name: 'Nick',
+    age: 24,
+    weekendAlarm: 'No alarms needed',
+    weekAlarm: 'Alarm set to 7AM',
+    sayHello: function() {
+      return `Hello, my name is ${this.name}`;
+    },
+    sayGoodbye() {
+      return 'Goodbye!';
+    }
+  };
+  
+  let friend = {
+      name:'Alec',
+  };
+  
+  friend.sayHello = person.sayHello;
+  
+  console.log(person.sayHello());
+  
+  person.hobbies = ['Rock Climbing', 'Cooking'];
+  person.hobbies = ['Rock Climbing'];
+  console.log(person.hobbies);
+  
+  
+  console.log(person['name']);
+  console.log(person['age']);
+  
+  let day = 'Tuesday';
+  let alarm;
+  
+  if (day === 'Saturday' || day === 'Sunday' ) {
+    alarm = 'weekendAlarm';
+  } else {
+    alarm = 'weekAlarm';
+  }
+  
+  console.log(person[alarm]);
+  console.log(friend.sayHello());
