875a39e409b608409bf4a51fea711dfc1954fbc4
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-29 14:33:52 -0600

    Sets percentage of browsers that support ES5 and percentage of browsers that support template literals to respective variables.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -1175,3 +1175,9 @@ let sauce = "Eggs and cheese"; // ES6 syntax
 
 // Template literals, like the one below, were introduced in ES6
 const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
+// Set the variable below to a number
+let esFivePercentageSupport = 96.85;
+
+// Set the variable below to a number
+let esSixTemplateLiterals = 88.72;
