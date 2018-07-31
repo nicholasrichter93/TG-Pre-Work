74c071f1765845736d4b00c5111375b4818e836d
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-30 19:31:43 -0600

    Lays groundwork for a fetch() POST request on an existing website.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -1866,4 +1866,39 @@ fetch('https://api-to-call.com/endpoint', {
 ).then(jsonResponse => {
   return jsonResponse;
 });
+
+// Information to reach API
+const apiKey = '8f6e865c088d480db2bbe70dca7a9663';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  fetch(url, {
+    method: 'POST',
+    headers:{
+  'Content-type': 'application/json',
+  'apikey': apiKey
+},
+    body: data
+  })
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild)
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
    
