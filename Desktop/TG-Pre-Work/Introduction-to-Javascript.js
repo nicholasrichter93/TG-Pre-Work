99e2afe300c23750df9e0382a123684dd93e0d44
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-30 19:44:05 -0600

    Creates async await get request.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -1986,3 +1986,16 @@ const displayShortUrl = (event) => {
 }
 
 shortenButton.addEventListener('click', displayShortUrl);
+
+const getData = async () => {
+    try {
+      const response = await fetch('https://api-to-call.com/endpoint');
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        return jsonResponse;
+      }
+      throw new Error('Request failed!');
+    } catch (error) {
+      console.log(error);	
+    }
+  }
