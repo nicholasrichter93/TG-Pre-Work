11ab1a3cec80dd2282943fd82cd264f8d7a745a3
Nicholas Richter <nicholasrichter93@gmail.com>
2018-07-30 18:58:34 -0600

    Creates boilerplate code for fetch() function.

--- Desktop/TG-Pre-Work/Introduction-to-Javascript.js
+++ Desktop/TG-Pre-Work/Introduction-to-Javascript.js
@@ -1770,3 +1770,14 @@ const changeButton = () => {
 };
 
 jsonButton.addEventListener('click', generateJson);
+
+fetch('https://api-to-call.com/endpoint').then(response => {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Request failed!');
+}, networkError => {
+  console.log(networkError.message);
+}).then(jsonResponse => {
+  return jsonResponse;
+});
